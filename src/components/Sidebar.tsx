8571c2dcac3edc8bbc9beaa401727b61fbde15d9
Dmitry Svechnikov <d.svechnikov@gmail.com>
2019-04-05 04:10:43 +0500

    add SidebarState

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,14 +3,19 @@ import * as React from 'react';
 import Logo from './Logo';
 import MainMenu from './MainMenu';
 
-class Sidebar extends React.Component {
+class Sidebar extends React.Component<Readonly<{}>, ISidebarState> {
     constructor(props: Readonly<{}>) {
-        super(props);        
+        super(props);
+        this.state = {
+            IsOpen: true
+        }
+
+        this.ToggleOpen = this.ToggleOpen.bind(this);
     }
 
     public render() {
         return (
-            <div className="sidebar open">
+            <div className={this.state.IsOpen ? "sidebar open" : "sidebar"}>
                 <div className="sidebar-content">
                     <Logo/>
                     <MainMenu/>
@@ -18,6 +23,13 @@ class Sidebar extends React.Component {
                 <div className="sidebar-toggle"><i id="sidebar-toggle-button" className="fas fa-equals"/></div>
             </div>
         );
+    }
+
+    private ToggleOpen() {
+        const newState: ISidebarState = {
+            IsOpen: !this.state.IsOpen
+        }
+        this.setState(newState)
     }
 }
 
---
+++ src/data-types/SidebarState.ts
@@ -0,0 +1,3 @@
+interface ISidebarState {
+    IsOpen: boolean
+}
