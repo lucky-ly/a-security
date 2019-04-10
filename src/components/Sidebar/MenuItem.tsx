6600e6af02bd5ba56b2ecf99360ffd1f3550a706
Dmitry Svechnikov <d.svechnikov@gmail.com>
2019-04-10 16:06:53 +0500

    add title to menu items

--- src/components/Sidebar/MenuItem.tsx
+++ src/components/Sidebar/MenuItem.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import IMenuItem from 'src/data-types/IMenuItem';
 
-function MenuItem({ link: link, text: text }: IMenuItem) {
+function MenuItem({ link: link, text: text, title: title }: IMenuItem) {
     return (
         <div className="main-menu__item">
-            <a href={link}>{text}</a>
+            <a href={link} title={title ? title : text}>{text}</a>
         </div>
     );
 }
--- src/data-types/IMenuItem.ts
+++ src/data-types/IMenuItem.ts
@@ -1,4 +1,6 @@
-export default interface IMenuItem {
+import ITitled from './ITitled';
+
+export default interface IMenuItem extends ITitled {
     link: string;
     text: string;
 }
