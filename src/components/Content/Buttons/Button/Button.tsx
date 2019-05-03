44610163aadfb8bee0b15d90bd4a6253f8784162
Dmitry Svechnikov <d.svechnikov@gmail.com>
2019-05-03 19:42:23 +0500

    add color to Button

--- src/components/Content/Buttons/Button/Button.tsx
+++ src/components/Content/Buttons/Button/Button.tsx
@@ -7,12 +7,17 @@ export interface IButtonProps {
     url: string;
     text: string;
     type?: string;
+    color?: string;
 }
 
 export const Button = (props: IButtonProps) => {
+    const style = {
+        color: props.color ? props.color : "inherit",
+    };
+    
     return (
         <a className={`button ${props.type ? `button_type-${props.type}` : ""}`} href={props.url}>
-            <span className="button__text">{props.text}</span>
+            <span className="button__text" style={style}>{props.text}</span>
             {props.children}
         </a>);
 };
